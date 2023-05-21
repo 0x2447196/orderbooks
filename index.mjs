7da3d9fc72c1087eaba79bab23f6b78d01b79c00
0x2447196 <2447196@gmail.com>
2023-05-20 20:46:57 -0400

    take an argument

--- index.mjs
+++ index.mjs
@@ -1,9 +1,12 @@
 import { OrderBook } from "./lib/orderbook.mjs"
 import { BinanceOrderbook } from "./lib/binance_orderbook.mjs"
 
-const binance = new BinanceOrderbook('1000pepeusdt', new OrderBook());
+const symbol = process.argv[2] || 'btcusdt'; 
+const binance = new BinanceOrderbook(symbol, new OrderBook());
+
 binance.orderBook.on('update', (item) => {
   const bP = binance.orderBook.getBookPressure();
   process.stdout.write(`book pressure: ${bP.toFixed(7)}\r`);
 });
+
 binance.start();
